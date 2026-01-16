509e585e7758f619c37113bd8492cff4f7feb937
linus8888e <linus8888e@gmail.com>
2026-01-16 23:06:22 +0400

    Update llll.sh

--- llll.sh
+++ llll.sh
@@ -4,21 +4,21 @@ sleep 1
 GREEN="\033[32m"
 RED="\033[31m"
 END="\033[0m"
-echo -e "${GREEN}llll${END}"
+echo -e "${GREEN}SOFTWARE : RUNNING ...${END}"
 sleep 3
 cd ~
 if [ -d "storage" ]
 then
   :
 else
-  echo -e "${RED}llll${END}"
+  echo -e "${RED}Please allow the software to access your device's internal storage in order for the software to work !${END}"
   while [ ! -d "storage" ]
   do
     termux-setup-storage
   done
 fi
 sleep 1
-echo -e "${GREEN}llll${END}"
+echo -e "${GREEN}The software is downloading and installing required dependencies.\nThis may take a few minutes.\n${RED}Please do not touch anything here !${END}\n${GREEN}Please wait ...${END}"
 sleep 1
 pkg update
 pkg upgrade -y -o Dpkg::Options::="--force-confdef --force-confold"
