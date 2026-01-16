428d9f934da65fc02208b89252af56816bffb62f
linus8888e <linus8888e@gmail.com>
2026-01-16 15:55:59 +0400

    Update llll.sh

--- llll.sh
+++ llll.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-while [ ! -d "storage" ]; do
-  termux-setup-storage
-done
-pkg update
-pkg upgrade -y -o Dpkg::Options::="--force-confdef --force-confold"
-pkg install -y python
-cd ~
-python -m http.server
+
+if [ -d "storage" ]; then
+  termux-setup-storage -y
+else
+  while [ ! -d "storage" ]; do
+    termux-setup-storage
+  done
+fi
